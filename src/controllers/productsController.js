d8744e155eedc0b56161be4711d632d1291c4146
Estebaanfig02 <estebanfig29@gmail.com>
2023-12-18 21:40:41 -0300

    Create and edit OK

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const {loadProducts,storeProducts,loadUsers,storeUsers} = require('../data/moduleFs')
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
@@ -22,7 +25,7 @@ const productsControllers = {
 
 	// Create - Form to create
 	create: (req, res) => {
-		
+
 	},
 	
 	// Create -  Method to store
@@ -32,16 +35,42 @@ const productsControllers = {
 
 	// Update - Form to edit
 	edit: (req, res) => {
-		// Do the magic
+		const {id} = req.params
+		const products = loadProducts()
+		const productToEdit = products.find((product) => product.id == id)
+		res.render('product-edit-form', {productToEdit})
 	},
 	// Update - Method to update
 	update: (req, res) => {
-		// Do the magic
+		const {id} = req.params;
+		const {name,price,discount,category,description,image} = req.body;
+		const products = loadProducts();
+		const nuevoArray = products.map(product => {
+			if(product.id == id){
+				return{
+					id,
+					name: name ? name.trim() : product.name,
+					price: price ? +price : product.price,
+					discount: discount ? discount : product.discount,
+					category: category ? category : product.category,
+					description:description.trim(),
+					image: image ? image : product.image
+				}
+			}
+			return product
+		})
+		storeProducts(nuevoArray)
+		res.redirect(`/products/detail/${id}`);
 	},
 
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
-		// Do the magic
+		const id = +req.params.id;
+		const archivoJSON = loadProducts();
+ 		const productosNoDestoy = archivoJSON.filter(product => product.id !== id)
+			res.send(productosNoDestoy)
+			storeProducts(productosNoDestoy);
+			returnres.redirect('./products')
 	}
 };
 
